847ca888e4f7d7ac20bd36a7c70eb3e1bf4d5e53
Andras Serfozo <subztep@gmail.com>
2021-02-22 12:23:15 +0000

    Clean up for build :sweat_drops:

--- src/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-export const calcPercent = (whole: number, amount: number): number => (amount / whole) * 100
-
-export const getRandomItem = <T>(items: readonly T[]): T => items[Math.floor(Math.random() * items.length)]
-
-export const rotate2d = (matrix: number[][]): number[][] => matrix[0].map((_, index) => matrix.map(row => row[index]).reverse())
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite"
-import pugPlugin from "vite-plugin-pug"
-
-console.log()
-
-export default defineConfig({
-  plugins: [pugPlugin({ pretty: process.env.NODE_ENV !== "production" })],
-})
