ac9e7bfde46333808a5c35d5740ac085c1ae3f91
Andras Serfozo <subztep@gmail.com>
2021-02-25 16:54:54 +0000

    Up :shipit:

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite"
 import pugPlugin from "vite-plugin-pug"
 
-console.log()
-
 export default defineConfig({
   plugins: [pugPlugin({ pretty: process.env.NODE_ENV !== "production" })],
   base: "./"
